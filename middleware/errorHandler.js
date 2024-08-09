e80b0228bb168c2552b60278856fdda93c61e4df
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-09 17:26:41 +0530

    add mycontact-backend master

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,8 +1,10 @@
+const { constants } = require("../constants");
+
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
 
     switch (statusCode) {
-        case 400:
+        case constants.VALIDATION_ERROR:
             res.json({
                 title: "Validation Failed",
                 message: err.message,
@@ -10,9 +12,33 @@ const errorHandler = (err, req, res, next) => {
             });
             break;
 
-        case 404:
+        case constants.NOT_FOUND:
             res.json({
                 title: "Resource Not Found",
+                message: err.message,
+                stackTrace: err.stack,
+            });
+            break;
+
+        case constants.UNAUTHORIZED:
+            res.json({
+                title: "Unauthorized",
+                message: err.message,
+                stackTrace: err.stack,
+            });
+            break;
+
+        case constants.FORBIDDEN:
+            res.json({
+                title: "Forbidden",
+                message: err.message,
+                stackTrace: err.stack,
+            });
+            break;
+
+        case constants.SERVER_ERROR:
+            res.json({
+                title: "Server Error",
                 message: err.message,
                 stackTrace: err.stack,
             });
