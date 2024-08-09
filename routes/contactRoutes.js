faa0a14d4197e40773a235b7d5dafd576c806fbd
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-09 11:30:25 +0530

    add mycontact-backend master

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+// GET request to retrieve all contacts
 router.get("/", (req, res) => {
     console.log("GET request to the root route");
     res.status(200).json({ message: "get all contacts" });
+});
+
+// POST request to create a new contact
+router.post("/", (req, res) => {
+    console.log("POST request to create a new contact");
+    res.status(201).json({ message: "create a new contact" });
+});
+
+// PUT request to update an existing contact
+router.put("/:id", (req, res) => {
+    console.log(`PUT request to update contact with id ${req.params.id}`);
+    res.status(200).json({ message: `update contact with id ${req.params.id}` });
+});
+
+// DELETE request to delete a contact
+router.delete("/:id", (req, res) => {
+    console.log(`DELETE request to delete contact with id ${req.params.id}`);
+    res.status(200).json({ message: `delete contact with id ${req.params.id}` });
 });
 
 module.exports = router;
