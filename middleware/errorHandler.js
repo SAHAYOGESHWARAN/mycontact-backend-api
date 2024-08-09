d4f9d0d4b6ce2b5ef1885dabf207c60c3b82914f
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-09 17:14:52 +0530

    add mycontact - backend master

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,31 @@
-const errorHandler = (err,req,res,next) => {
-    const statusCode = res.statusCode ? res.statusCode:500;
-    res.json({message:err.message,stackTrace:err.stack});
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+
+    switch (statusCode) {
+        case 400:
+            res.json({
+                title: "Validation Failed",
+                message: err.message,
+                stackTrace: err.stack,
+            });
+            break;
+
+        case 404:
+            res.json({
+                title: "Resource Not Found",
+                message: err.message,
+                stackTrace: err.stack,
+            });
+            break;
+
+        default:
+            res.status(statusCode).json({
+                title: "An error occurred",
+                message: err.message,
+                stackTrace: err.stack,
+            });
+            break;
+    }
 };
 
 module.exports = errorHandler;
