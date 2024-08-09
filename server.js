d96121cf4914d9d448aab2fa7efdb84fa5b0cc5a
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-09 10:24:48 +0530

    add mycontact master

---
+++ server.js
@@ -0,0 +1,19 @@
+const express =require("express");
+const app = express();
+
+
+
+
+
+
+const port = 6000;
+
+
+
+
+
+
+
+app.listen(port,() => {
+    console.log(`Server is running on port ${port}`)
+});
