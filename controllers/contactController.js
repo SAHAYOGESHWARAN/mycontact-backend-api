df9b0abd020e5686380b644cd04a073793eba460
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-09 17:35:52 +0530

    add mycontact-backend master

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,15 +1,16 @@
+const asyncHandler = require("express-async-handler");
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
-const getContacts = async(req, res) => {
+const getContacts =asyncHandler( async(req, res) => {
     console.log("GET request to get all contacts");
     res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 //@desc Create a new contact
 //@route POST /api/contacts
 //@access public
-const createContact = async(req, res) => {
+const createContact = asyncHandler(async(req, res) => {
     console.log("the request body is:",req.body);
     const{name,email,phone} = req.body;
     if(!name || !email || !phone) {
@@ -17,34 +18,34 @@ const createContact = async(req, res) => {
         throw new Error("All fields are mandatory !");
     }
     res.status(201).json({ message: "Create a new contact" });
-};
+});
 
 //@desc Get a contact by ID
 //@route GET /api/contacts/:id
 //@access public
-const getContactById = async(req, res) => {
+const getContactById = asyncHandler(async(req, res) => {
     const { id } = req.params;
     console.log(`GET request to get contact with ID ${id}`);
     res.status(200).json({ message: `Get contact with ID ${id}` });
-};
+});
 
 //@desc Update a contact by ID
 //@route PUT /api/contacts/:id
 //@access public
-const updateContact =async (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
     const { id } = req.params;
     console.log(`PUT request to update contact with ID ${id}`);
     res.status(200).json({ message: `Update contact with ID ${id}` });
-};
+});
 
 //@desc Delete a contact by ID
 //@route DELETE /api/contacts/:id
 //@access public
-const deleteContact = async(req, res) => {
+const deleteContact = asyncHandler(async(req, res) => {
     const { id } = req.params;
     console.log(`DELETE request to delete contact with ID ${id}`);
     res.status(200).json({ message: `Delete contact with ID ${id}` });
-};
+});
 
 // Export all functions
 module.exports = {
