137122737abcecda323cffd7b70729bf36475420
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-10 17:55:18 +0530

    add mycontact-backend master

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const{ registerUser,currentUser,loginUser} = require("../controllers/userController");
+const validateToken = require("../middleware/validateTokenHandler");
 const router = express.Router();
 
 router.post("/register",registerUser);
 
 router.post("/login", loginUser);
-router.post("/current", currentUser);
+router.post("/current",validateToken, currentUser);
 
 module.exports = router;
