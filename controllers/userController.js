bf44662b3b711ff85023ea4e04cf7f5169b7fb1f
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-10 11:20:09 +0530

    add mycontact-backend master

--- controllers/userController.js
+++ controllers/userController.js
@@ -31,8 +31,20 @@ const registerUser = asyncHandler(async (req, res) => {
         password: hashedPassword,
     });
 
+   
     console.log(`User created: ${user}`);
-    res.status(201).json({ message: "User registered successfully", user });
+
+    // Check if the user was created successfully
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
 });
 
 // @desc Login a user
